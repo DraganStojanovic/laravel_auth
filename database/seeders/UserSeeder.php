b6132ba3493835501d314924f928d8087abab2db
DraganStojanovic <draganstojanovic3@gmail.com>
2023-09-15 13:45:42 +0200

    * DB:Seed 500 to DB

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,10 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Dragan Smith',
-            'email' => 'dragansw@gmail.com',
-            'password' => Hash::make('1111'),
-        ]);
+//        User::create([
+//            'name' => 'Dragan Smith',
+//            'email' => 'dragansw@gmail.com',
+//            'password' => Hash::make('1111'),
+//        ]);
+        $faker = Factory::create();
+        for( $i = 0; $i < 500; $i++ )
+        {
+            User::create([
+                'name' =>  $faker->name,
+                'email' => $faker->email,
+                'password' => Hash::make('12345678')
+            ]);
+        }
     }
 }
