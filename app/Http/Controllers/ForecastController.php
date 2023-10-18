2318ad07b8028924f0b8fc3ff6404110fbacb602
DraganStojanovic <draganstojanovic3@gmail.com>
2023-10-18 13:43:25 +0200

    * Provera velicine slova u stringu!

--- app/Http/Controllers/ForecastController.php
+++ app/Http/Controllers/ForecastController.php
@@ -9,10 +9,11 @@ class ForecastController extends Controller
     public function index($city)
     {
         $forecast = [
-            'Beograd' => [23, 24, 25, 18, 11],
-            'Sarajevo' => [20, 22, 21, 25, 13],
+            'beograd' => [23, 24, 25, 18, 11],
+            'sarajevo' => [20, 22, 21, 25, 13],
         ];
 
+        $city = strtolower($city);
         if(!array_key_exists($city, $forecast)) // Provera da li postoji grad!
         {
             die("Ovaj grad ne postoji!");
